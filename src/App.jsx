04503cc1656e688649ff03d488788bf64067765f
d5u <danielsu@protonmail.ch>
2021-09-01 22:13:54 -0700

    *Add fetchData function *Dynamically set values for select forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
+import axios from 'axios';
+
 import Search from '../src/Components/Search';
 import Results from '../src/Components/Results';
 
 const App = () => {
+  let [reps, setReps] = useState('');
+  let [sens, setSens] = useState('');
+
+  const fetchData = (state, type) => {
+    switch (type) {
+      case 'rep':
+        axios.get(`http://localhost:3001/representatives/${state}`)
+          .then(({ data }) => console.log(data))
+          .catch(err => console.log('Request to representatives API endpoint failed with ' + err));
+      break;
+      case 'sen':
+        axios.get(`http://localhost:3001/senators/${state}`)
+          .then(({ data }) => console.log(data))
+          .catch(err => console.log('Request to senators API endpoint failed with ' + err))
+        break;
+      default:
+        console.log('Missing type');
+    }
+  }
+
+
   return (
     <>
       <header>
         <h1>Who Is My Representative?</h1>
       </header>
       <main>
-        <Search />
+        <Search fetchData={fetchData} />
         <Results />
       </main>
     </>
--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,21 +1,30 @@
+import { useState } from 'react';
 import states from '../data/states.json';
 
-const Search = () => {
+const Search = ({ fetchData }) => {
+  let [state, setState] = useState('AL');
+  let [type, setType] = useState('rep');
+
   return (
     <div>
       <h4>State:</h4>
-      <select name="states">
+      <select value={state} onChange={e => setState(e.target.value)}>
         {
           states.map(state => 
-            <option key={state.abbreviation}>{state.name}</option>)
+            <option
+              key={state.abbreviation}
+              value={state.abbreviation}
+            >
+              {state.name}
+            </option>)
         }
       </select>
       <h4>Type:</h4>
-      <select name="type">
+      <select value={type} onChange={e => setType(e.target.value)}>
         <option value='rep'>Representatives</option>
         <option value='sen'>Senators</option>
       </select>
-      <button>Search</button>
+      <button onClick={e => {fetchData(state, type)}}>Search</button>
     </div>
   )
 }
